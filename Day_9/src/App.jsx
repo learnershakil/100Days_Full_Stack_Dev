daeffd7974cdebed7733e295ce62d02827b4b558
learnershakil <learnershakil@gmail.com>
2024-07-12 06:35:36 +0530

    revise useState

---
+++ Day_9/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [value, setValue] = useState('Learner')
+  function randomValue() {
+    setValue(Math.random())
+  }
+
+  return (
+    <>
+      <Header data={value} /> 
+      <Header data='React' />
+      <button onClick={randomValue}>random value</button>
+    </>
+  )
+}
+
+function Header({ data }) {
+  return (
+    <h2>Hey I'm {data}</h2>
+  )
+}
+
+export default App
