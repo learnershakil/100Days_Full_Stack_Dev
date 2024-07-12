90a167d8083f7c6c3ecefdd53eb23d02051e0763
learnershakil <learnershakil@gmail.com>
2024-07-13 03:04:47 +0530

    useMemo

--- Day_9/src/App.jsx
+++ Day_9/src/App.jsx
@@ -1,33 +1,46 @@
+import React, {memo} from 'react'
 import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [value, setValue] = useState('Learner')
+  function randomValue() {
+    setValue(Math.floor(Math.random()*100)+1)
+    // setValue('Learner')
+  }
 
   return (
     <>
       <Header data='React' />
-      <HeaderwithButton />
+      <DynamicHeader data={value} /> 
+      <button onClick={randomValue}>random value</button>
     </>
   )
 }
 
-function HeaderwithButton() {
-  const [value, setValue] = useState('Learner')
-  function randomValue() {
-    setValue(Math.floor(Math.random()*100)+1)
-  }
-  return(
-    <>
-    <Header data={value} /> 
-    <button onClick={randomValue}>random value</button>
-    </>
-  )
-}
+// function HeaderwithButton() {
+//   const [value, setValue] = useState('Learner')
+//   function randomValue() {
+//     setValue(Math.floor(Math.random()*100)+1)
+//   }
+//   return(
+//     <>
+//     <Header data={value} /> 
+//     <button onClick={randomValue}>random value</button>
+//     </>
+//   )
+// }
 
 function Header({ data }) {
   return (
     <h2>Hey I'm {data}</h2>
   )
 }
+
+const DynamicHeader = React.memo(({data})=> {
+  return(
+    <h2>Hey I'm {data}</h2> 
+  )
+})
 
 export default App
